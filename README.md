ef37392be02ec6e1804222af698b98d0920525bb
Roger Knop <roger.knop@sap.com>
2022-12-19 14:54:10 +0100

    Import test & config redesign

--- README.md
+++ README.md
@@ -7,12 +7,20 @@ node src/index.js [configfilename without .json]
 
 npm run start -> Nodemon mit Watch über Änderungen in src oder config.
 
+
+## ToDo
+* Lochrastersockel mehr Parameter: größere Lücke
+* Shelly Socket Types
+* Array für Löcher
+
 ## Installation & Doku
+Doku
 * https://github.com/jscad/OpenJSCAD.org
 * https://openjscad.xyz/docs/
 
 npm install -D @jscad/cli
 
-## JSCAD direkt ausführen
+## Infos
+### JSCAD direkt ausführen
 * npx jscad src/index.js -o export/result.jscad.json
 * Über nodemon: npx nodemon -w ./src --exec npx jscad src/index.js -o export/result.jscad.json
--- src/box.js
+++ src/box.js
@@ -5,6 +5,12 @@ const fs = require('fs');
 const jsonSerializer = require('@jscad/json-serializer');
 const objSerializer = require('@jscad/obj-serializer');
 
+const { roundedCuboid, cuboid, rectangle, roundedRectangle, cylinder } = jscad.primitives;
+const { subtract, union } = jscad.booleans;
+const { center, rotate } = jscad.transforms;
+const { degToRad } = jscad.utils;
+const { extrudeLinear } = jscad.extrusions
+
 class Box {
     core;
 
@@ -13,36 +19,26 @@ class Box {
     height = 0;
     thickness = 2;
     rounded = 0.25;
-    ventilationCount = 0;
-    sockelType = 1;
-    lid_kulanz = 0.3; //0.3 Press / 0.4 minimal spiel aber klickt gut
-    clickRadiusPercent = 0.4; //Prozent von Thickness
-    clickHeightCorpus = 9;
-    clickHeightLid = 10;
-    ventilationLengthPercent = 0.8;
-    socketHeight = 5;
-    socketOuterRadius = 2;
-    socketInnerRadius = 1;
+    socket = {};
+    click = {}; 
+    ventilation = {};
 
+    //-----------------------------------------------------------------------------------------------------------------
     constructor(core, params) {
         this.core = core;
         Object.assign(this, params);
     }
     
+    //-----------------------------------------------------------------------------------------------------------------
     corpus() {
-        const { roundedCuboid, cuboid, cylinder } = jscad.primitives;
-        const { subtract, union } = jscad.booleans;
-        const { center, rotate } = jscad.transforms;
-        const { degToRad } = jscad.utils;
-
         let result = roundedCuboid({size: [this.width, this.depth, this.height+this.rounded], center: [0, 0, (this.height/2)], roundRadius: this.rounded});
         result = subtract(result,  roundedCuboid({size: [this.width-(this.thickness*2), this.depth-(this.thickness*2), this.height+this.rounded], center: [0, 0, (this.height/2)+(this.thickness*2)], roundRadius: this.rounded}));
         let notround = cuboid({size: [this.width, this.depth, this.rounded]});
         notround = center({relativeTo: [0, 0, this.height]}, notround);
         result = subtract(result, notround);
 
-        let r = ((this.thickness/2) * this.clickRadiusPercent) - 0.05; 
-        let h = this.clickHeightCorpus;
+        let r = ((this.thickness/2) * this.click.radiusPercent) - 0.05; 
+        let h = this.click.heightCorpus;
         let clickDepth = (this.depth/2) - this.thickness;
         let clickWidth = (this.width/2) - this.thickness;
         let clickHeight = this.height - r - ((this.thickness -(2*r)) / 2);
@@ -60,36 +56,12 @@ class Box {
         c = center({relativeTo: [-clickWidth, 0, clickHeight]}, cyl);
         result = union(result, c);
 
-        if (this.sockelType>0) {
-            if (this.sockelType==1) {
-                //Für generische schneidbare Platinen
-                let sockel_r = this.socketOuterRadius;
-                let sockel_h = this.socketHeight;
-                let sockel = cylinder( {radius: sockel_r, height: sockel_h});
-                let sockel_innen_r = this.socketInnerRadius;
-                let sockel_innen = cylinder( {radius: sockel_innen_r, height: sockel_h});
-                let sockel_height = (this.thickness/2)+this.thickness+(sockel_h/2);
-                sockel = subtract(sockel, sockel_innen);
-                sockel = center({relativeTo: [0, 0, sockel_height]}, sockel);
-
-                //Min 2mm Abstand vom Rand
-                let sockelDepth = (this.depth/2) - this.thickness - sockel_r - 2; 
-                let sockelWidth = (this.width/2) - this.thickness - sockel_r - 2;
-
-                //sockelDepth und sockelWidth auf Lochabstand der Platine normalisieren
-                let lochabstand = 2.54;
-                sockelDepth = (Math.floor( (sockelDepth*2) / lochabstand) * lochabstand) / 2;
-                sockelWidth = (Math.floor( (sockelWidth*2) / lochabstand) * lochabstand) / 2;
-
-                let s = center({relativeTo: [sockelWidth, sockelDepth, sockel_height]}, sockel);
-                result = union(result, s);
-                s = center({relativeTo: [sockelWidth, -sockelDepth, sockel_height]}, sockel);
-                result = union(result, s);
-                s = center({relativeTo: [-sockelWidth, -sockelDepth, sockel_height]}, sockel);
-                result = union(result, s);
-                s = center({relativeTo: [-sockelWidth, sockelDepth, sockel_height]}, sockel);
-                result = union(result, s);
-            }
+        //Sockets
+        if (this.socketBreadboard?.active) {
+            result = this.addSocketBreadboard(result);
+        }
+        if (this.socketShelly?.active) {
+            result = this.addSocketShelly(result);
         }
 
         if (this.outerScrews) {
@@ -115,34 +87,28 @@ class Box {
             result = union(result, os);
             os = center({relativeTo: [-osWidth, osDepth, osHeight]}, outer_screw);
             result = union(result, os);
-        
         }
 
         return result;
     }
 
-    lid() {
-        const { extrudeLinear } = jscad.extrusions
-        const { roundedCuboid, cuboid, rectangle, roundedRectangle, cylinder } = jscad.primitives
-        const { subtract, union } = jscad.booleans;
-        const { center, rotate } = jscad.transforms;
-        const { degToRad } = jscad.utils;
-    
+    //-----------------------------------------------------------------------------------------------------------------
+    lid() {    
         let result = roundedCuboid({size: [this.width, this.depth, this.thickness], center: [0, 0, (this.thickness/2)], roundRadius: this.rounded});
         result = center({relativeTo: [0, 0, this.thickness/2]}, result);
         let notround = cuboid({size: [this.width, this.depth, this.rounded]});
         notround = center({relativeTo: [0, 0, -(this.rounded/2)+this.thickness]}, notround);
         result = subtract(result, notround);
     
-        let lid = roundedRectangle({size: [this.width-(this.thickness*2)-this.lid_kulanz, this.depth-(this.thickness*2)-this.lid_kulanz], roundRadius: 2})
+        let lid = roundedRectangle({size: [this.width-(this.thickness*2)-this.click.lidReduce, this.depth-(this.thickness*2)-this.click.lidReduce], roundRadius: 2})
         lid = extrudeLinear({height: this.thickness}, lid);
         lid = center({relativeTo: [0, 0, (this.thickness/2)+this.thickness-this.rounded]}, lid);
         result = union(result, lid);
     
-        let r = (this.thickness/2) * this.clickRadiusPercent;
-        let h = this.clickHeightLid;
-        let clickWidth = (this.width/2) - this.thickness - this.lid_kulanz;
-        let clickDepth = (this.depth/2) - this.thickness - this.lid_kulanz;
+        let r = (this.thickness/2) * this.click.radiusPercent;
+        let h = this.click.heightLid;
+        let clickWidth = (this.width/2) - this.thickness - this.click.lidReduce;
+        let clickDepth = (this.depth/2) - this.thickness - this.click.lidReduce;
         let clickHeight = (this.thickness*2) - r - ((this.thickness -(2*r)) / 2) - this.rounded;
 
         let c;
@@ -170,17 +136,18 @@ class Box {
         c = center({relativeTo: [-clickWidth, 0, clickHeight]}, cyl);
         result = subtract(result, c);
     
-        if (this.ventilationCount>0) {
-            let ventilationWidth = 1;
-            let availDepth = this.depth-(this.thickness*2)-(this.lid_kulanz*2);
-            let availWidth = this.width-(this.thickness*2)-(this.lid_kulanz*2);
-            let ventilationLength = availDepth * this.ventilationLengthPercent;
-            let gap = (availWidth / (this.ventilationCount + 1));
+        if (this.ventilation.count>0) {
+            let ventilationWidth = this.ventilation.width;
+            let ventilationShift = this.ventilation.shift || 0;
+            let availDepth = this.depth-(this.thickness*2)-(this.click.lidReduce*2);
+            let availWidth = this.width-(this.thickness*2)-(this.click.lidReduce*2);
+            let ventilationLength = availDepth * this.ventilation.lengthPercent;
+            let gap = (availWidth / (this.ventilation.count + 1));
             let currPos = -(availWidth / 2) ;
             let gapCuboid = cuboid({size: [ventilationWidth, ventilationLength, this.thickness * 5]});
-            for (let i = 1; i<=this.ventilationCount; i++) {
+            for (let i = 1; i<=this.ventilation.count; i++) {
                 currPos += gap;
-                gapCuboid = center({relativeTo: [currPos, 0, 0]}, gapCuboid);
+                gapCuboid = center({relativeTo: [currPos, ventilationShift, 0]}, gapCuboid);
                 result = subtract(result, gapCuboid);
             }
         }
@@ -188,6 +155,64 @@ class Box {
         return result;
     }
 
+    //-----------------------------------------------------------------------------------------------------------------
+    addSocketBreadboard(result) {
+        //Für generische schneidbare Platinen
+        this.socket = this.socketBreadboard;
+        let socket_r = this.socket.outerRadius;
+        let socket_h = this.socket.height;
+        let socket = cylinder( {radius: socket_r, height: socket_h});
+        let socket_innen_r = this.socket.innerRadius;
+        let socket_innen = cylinder( {radius: socket_innen_r, height: socket_h});
+        let socket_height = (this.thickness/2)+this.thickness+(socket_h/2);
+        socket = subtract(socket, socket_innen);
+        socket = center({relativeTo: [0, 0, socket_height]}, socket);
+
+        //Min 2mm Abstand vom Rand
+
+        let socketDepth; 
+        let socketWidth;
+
+        if (this.socket.maxDepth>0) { socketDepth = this.socket.maxDepth / 2; }
+        else { socketDepth = (this.depth/2) - this.thickness - socket_r - 2; }
+        if (this.socket.maxWidth>0) { socketWidth = this.socket.maxWidth / 2; }
+        else { socketWidth = (this.width/2) - this.thickness - socket_r - 2; }
+
+        //socketDepth und socketWidth auf Lochabstand der Platine normalisieren
+        let lochabstand = 2.54;
+        socketDepth = (Math.floor( (socketDepth*2) / lochabstand) * lochabstand) / 2;
+        socketWidth = (Math.floor( (socketWidth*2) / lochabstand) * lochabstand) / 2;
+        let shiftDepth = this.socket.shiftDepth;
+        let shiftWidth = this.socket.shiftWidth;
+
+        let s = center({relativeTo: [socketWidth+shiftWidth, socketDepth+shiftDepth, socket_height]}, socket);
+        result = union(result, s);
+        s = center({relativeTo: [socketWidth+shiftWidth, -socketDepth+shiftDepth, socket_height]}, socket);
+        result = union(result, s);
+        s = center({relativeTo: [-socketWidth+shiftWidth, -socketDepth+shiftDepth, socket_height]}, socket);
+        result = union(result, s);
+        s = center({relativeTo: [-socketWidth+shiftWidth, socketDepth+shiftDepth, socket_height]}, socket);
+        result = union(result, s);
+        
+        return result;
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
+    addSocketShelly(result) {
+        //https://www.thingiverse.com/thing:4842901
+        
+        /*
+        Performance grottenschlecht!!!
+        const objDeserializer = require('@jscad/obj-deserializer');
+        const rawData = fs.readFileSync('./src/import/test.obj', 'utf8');
+        let rgbShelly = objDeserializer.deserialize({ output: 'geometry', addMetaData: false }, rawData);
+        result = union(result, rgbShelly);
+        */
+
+        return result;
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------
     export() {
         let corpus = this.corpus();
 
